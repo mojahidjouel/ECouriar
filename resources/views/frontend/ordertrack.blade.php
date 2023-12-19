148caf9644f59f999764072485e96daf9dcf1646
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-19 22:39:57 +0600

    track

--- resources/views/frontend/ordertrack.blade.php
+++ resources/views/frontend/ordertrack.blade.php
@@ -12,7 +12,22 @@
             <h6 class="text-danger">ID:</h6>
             <h4 class="card-title text-info"></h6>
           <?php print_r($data); ?>
-            
+          {{--<div class="card-body bg-white">
+            <h6 class="text-danger">User Id: </h6>
+            <h4 class="card-title text-info">{{$data->user?->name}}</h4>
+
+            <h6 class="text-danger">Shipment Id: </h6>
+            <h4 class="card-subtitle mb-2 text-info">{{$data->shipment?->deliveryman?->name}}</h4>
+
+            <h6 class="text-danger">Comment: </h6>
+            <h4 class="card-subtitle mb-2 text-info">{{$data->comment}}</h4>
+
+            <h6 class="text-danger">Status: </h6>
+            <h4 class="card-subtitle mb-2 text-info">
+            @if($data->status == 0) {{__('Pending') }} @elseif($data->status == 1) {{__('Picked Up') }} @else {{__('Delivered') }} @endif
+            </h4>
+
+         </div> --}}
          </div>
         </div>
         <div class="p-2 w-20 mx-auto">
